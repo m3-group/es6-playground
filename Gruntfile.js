a972dbcbed32e1d89fdb872b335a1e5e3b6a1683
netpoetica <krosenberg@m3-health.com>
2014-03-24 09:04:44 -0400

    Functional build, updated instructions from README, new node pkgs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,106 @@
-module.exports = function(grunt) {
+(function(module){
+  'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    meta: {
-      sourceDir: 'src',
-      testDir: 'test',
-      buildDir: 'build'
-    },
-    traceur: {
+  var extend = require('node.extend'),
+      BASE_PORT = 8089,
+      TEST_PORT = 8090;
+
+  module.exports = function(grunt) {
+
+    var base_server = {
       options: {
+        base: './<%= meta.buildDir %>',
+        hostname: '127.0.0.1',
+        port: BASE_PORT,
+        keepalive: true,
+        middleware: function(connect, options) {
+          return [
+            // Serve static files.
+            connect.static(String(options.base) /* Fixes bug: https://github.com/gruntjs/grunt-contrib-connect/issues/41 */),
+
+            // Make empty directories browsable.
+            connect.directory(options.base)
+          ];
+        }
+      }
+    };
+
+    // Dupe server config for testing on diff port (able to keep grunt server running and test)
+    var test_server = extend(true, {}, base_server);
+    test_server.options.base = '.';
+    test_server.options.keepalive = false;
+    test_server.options.port = TEST_PORT;
+
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      meta: {
+        sourceDir: 'src',
+        jsDir: 'js',
+        testDir: 'test',
+        buildDir: 'build'
       },
-      build: {
-        files: {
-          '<%= meta.buildDir %>/<%= pkg.name %>.js': ['<%= meta.sourceDir %>/**/*.js'],
+      connect: {
+        server: base_server,
+        test: test_server
+      },
+      copy: {
+        index: {
+          src: '<%= meta.sourceDir %>/index.html',
+          dest: '<%= meta.buildDir %>/index.html',
+        },
+      },
+      traceur: {
+        options: {
+        },
+        build: {
+          files: {
+            '<%= meta.buildDir %>/<%= meta.jsDir %>/<%= pkg.name %>.js': ['<%= meta.sourceDir %>/<%= meta.jsDir %>/**/*.js'],
+          }
         }
-      }
-    },
-    qunit: {
-      files: ['<%= meta.testDir %>/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', '<%= meta.sourceDir %>/**/*.js', '<%= meta.testDir %>/**/*.js'],
-      options: {
-        esnext: true, /* Important for ES6 Harmony not to throw errors */
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
+      },
+      qunit: {
+        files: ['<%= meta.testDir %>/**/*.html']
+      },
+      jshint: {
+        files: ['Gruntfile.js', '<%= meta.sourceDir %>/<%= meta.jsDir %>/**/*.js', '<%= meta.testDir %>/**/*.js'],
+        options: {
+          esnext: true, /* Important for ES6 Harmony not to throw errors */
+          globals: {
+            jQuery: true,
+            console: true,
+            module: true,
+            document: true
+          }
         }
+      },
+      watch: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['build']
       }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['build']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-traceur');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // A basic server for testing the app - this server only runs while grunt is running. Use keepalive: true for use as a development server
+    // https://github.com/gruntjs/grunt-contrib-connect
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('dev', ['watch']);
-  grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('build', ['jshint', 'qunit', 'traceur']);
+    // https://github.com/gruntjs/grunt-contrib-copy
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-traceur');
+    // ////////////////////// //
+    // END GRUNT TASK LOADING //
+    // ////////////////////// //
 
-};
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('dev', ['watch']);
+    grunt.registerTask('build', ['jshint', 'copy', 'traceur']);
+
+    grunt.registerTask('test', ['jshint', 'connect:test', 'qunit']);
+
+    // Spawn a basic HTTP server but keep it alive.
+    grunt.registerTask('serve', ['connect:server']);
+
+  };
+}(module));
--- README.md
+++ README.md
@@ -2,3 +2,43 @@ es6-playground
 ==============
 
 A sandbox for testing out ES6 Harmony (JavaScript) with a transpiler, and a collection of resources about it.
+
+The default grunt task will take everything in the src/js/ folder and transpile it to a JavaScript file included in index.html.
+
+## Quick Start
+Firstly, make sure you have Nodejs, npm, and grunt-cli installed.
+```
+node --version && npm --version && grunt --version
+
+// Output should be something like:
+// v0.10.21
+// 1.3.11
+// grunt-cli v0.1.11
+```
+If you're missing node, you can get it via homebrew or otherpackage managers:
+```
+brew install node
+```
+If you're missing grunt-cli, you can get it via npm:
+```
+npm install -g grunt-cli
+```
+Next, and lastly, you must install local packages via npm:
+```
+npm install
+```
+Once you have satisfied these dependencies, Copy and Paste the following code into terminal
+```
+grunt build && grunt serve
+```
+Now open your browser and navigate to http://127.0.0.1:8089 or http://localhost:8089
+
+## Building the Code
+Simply run
+```
+grunt
+```
+or 
+```
+grunt build
+```
